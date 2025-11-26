6ef54cc971cdf77a3fd845bfe2bf89b1665c2184
Trần Nguyễn Trung Hiếu <gauhieuminh@gmail.com>
2025-11-27 02:05:30 +0700

    Delete BaitapchuongVI directory

--- BaitapchuongVI/Exercise2.py
+++
@@ -1,7 +0,0 @@
-my_string = "Hello World! Python for Everybody."
-lower_string = my_string.lower()
-print(f"1. Chữ thường: {lower_string}")
-ends_with_exclamation = my_string.endswith('!')
-print(f"2. Kết thúc bằng '!'? {ends_with_exclamation}")
-replaced_string = my_string.replace('o', '@')
-print(f"3. Thay thế 'o' bằng '@': {replaced_string}")
